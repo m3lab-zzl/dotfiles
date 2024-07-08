314d5ed8923620130b14b019aa80441081a52819
zzl <zzl@HZW>
2024-07-08 14:44:09 +0800

    update mail.py

--- scripts/mail.py
+++ scripts/mail.py
@@ -1,3 +1,4 @@
+import getpass
 import os
 import smtplib
 from argparse import ArgumentParser
@@ -7,10 +8,11 @@ from email.mime.text import MIMEText
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
 ap = ArgumentParser("mail yourself")
-ap.add_argument("-p", help="password for email")
 ap.add_argument("-t", "--title", help="title")
 ap.add_argument("-c", "--content", help="content")
 args = ap.parse_args()
+
+passwd = getpass.getpass("password:")
 
 
 def send_email(subject, message, from_addr, to_addr, smtp_server, port, password):
@@ -32,5 +34,5 @@ send_email(
     to_addr="1091749869@qq.com",  # <---
     smtp_server="smtp.163.com",
     port=465,
-    password=args.p,
+    password=passwd,
 )
